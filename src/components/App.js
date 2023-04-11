e0f02e33ee94368ce403d5569670e794b11246ef
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-04-10 23:10:25 -0700

    greased hog status renders yes/no depending on greased value

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,52 @@ import HogsList from "./HogsList";
 
 function App() {
 	const [hogs, setHogs] = useState(allHogs)
+	const [nameChecked, setNameChecked] = useState("")
+	const [weightChecked, setWeightChecked] = useState("")
 
+	function handleShowGreasedHogs() {
+		const greasedHogsList = hogs.filter(hog => hog.greased)
+		setHogs(greasedHogsList)
+	}
+
+	function handleNameSort(e) {
+		setNameChecked(nameChecked => !nameChecked)
+		const nameSortedHogs = hogs.sort((a, b) => (a.name > b.name) ? 1 : (a.name < b.name) ? -1 :0)
+		setHogs(nameSortedHogs)
+		
+	}
+
+	function handleWeightSort(e) {
+		setWeightChecked(weightChecked => !weightChecked)
+		const weightSortedHogs = hogs.sort((a, b) => (a.weight > b.weight) ? 1 : (a.weight < b.weight) ? -1 : 0)
+		setHogs(weightSortedHogs)
+	}
 
 	return (
 		<div className="App">
 			<Nav />
+			<div>
+				<button onClick={handleShowGreasedHogs}>Show Greased Hogs</button>
+				<br/>
+				<h5>Filter piggies by</h5>
+				<input 
+					type="radio"
+					name="name"
+					value="name"
+					onChange={handleNameSort}
+					checked={nameChecked}
+				/>
+				<label> Name</label>
+				<br/>
+				<input 
+					type="radio"
+					name="weight"
+					value="weight"
+					onChange={handleWeightSort}
+					checked={weightChecked}
+				/>
+				<label> Weight</label>
+			</div>
 			<HogsList hogs={hogs}/>
 		</div>
 	);
--- src/components/HogDetails.js
+++ src/components/HogDetails.js
@@ -3,7 +3,7 @@ export default function HogDetails({hog}) {
     <div>
       <p>Specialty: {hog.specialty}</p>
       <p>Weight: {hog.weight}</p>
-      <p>Greased: {hog.greased}</p>
+      <p>Greased: {hog.greased ? "YES" : "NO"}</p>
       <p>Medal: {hog["highest medal achieved"]}</p>
 
     </div>
